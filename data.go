44945daea11dae21ef9abdf62657d4532646b03f
RoryNickolls <rorynickolls@gmail.com>
2020-01-31 14:37:13 +0000

    Add mini-batching

--- data.go
+++ data.go
@@ -1,10 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Data struct {
 	train []Record
 	test  []Record
 }
 
-func (d *Data) getTrainPartitions(n int) []Record {
-	return d.train
+func (d *Data) getMiniBatches(batchSize int) [][]Record {
+
+	if len(d.train)%batchSize != 0 {
+		fmt.Println("WARNING: Training data cannot be split evenly into batches of size", batchSize)
+	}
+
+	// Shuffle training data
+	rand.Shuffle(len(d.train), func(i, j int) { d.train[i], d.train[j] = d.train[j], d.train[i] })
+
+	var miniBatches [][]Record
+	for i := 0; i < len(d.train); i += batchSize {
+		miniBatches = append(miniBatches, d.train[i:i+batchSize])
+	}
+	return miniBatches
 }
--- main.go
+++ main.go
@@ -9,11 +9,15 @@ func main() {
 	fmt.Println("Training network with", len(data.train), "training instances and", len(data.test), "testing instances")
 
 	network := NewNetwork().WithLayer(784, 300, "sigmoid").WithLayer(300, 100, "sigmoid").WithLayer(100, 10, "softmax")
-	epochs := 50
+	epochs := 1000
 	eta := 0.01
+	batchSize := 100
 	for i := 0; i < epochs; i++ {
-		network.Train(data.train[:1000], eta)
 
+		miniBatches := data.getMiniBatches(batchSize)
+		for j := 0; j < len(miniBatches); j++ {
+			network.Train(miniBatches[j], eta)
+		}
 		loss, correct := network.Evaluate(data.test)
 
 		fmt.Printf("Epoch %.2d Loss %.4f Accuracy %.4f\n", i, loss, correct)
