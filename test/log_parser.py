2b2fd9f68a1852013b5e06507d775c693599c0d8
RoryNickolls <rorynickolls@gmail.com>
2020-05-04 10:40:13 +0100

    Update

--- test/log_parser.py
+++ test/log_parser.py
@@ -1,20 +1,15 @@
 import re
 
+# Cleans up junk from lines
 def clean_line(line):
     return " ".join(line.split(" ")[2:])
 
+# Ignores lines that do not begin with rx or tx
 def ignore_line(line):
     first_part = line[:2]
     return first_part != "tx" and first_part != "rx"
 
-def next_good_line(f):
-    for line in f:
-        clean = clean_line(line)
-        if ignore_line(clean):
-            continue
-        return clean
-    return None
-
+# Tests a log file with a regex pattern, returns true if it passes
 def check_log(name, pattern):
     with open(name) as f:
         cleaned = ""
